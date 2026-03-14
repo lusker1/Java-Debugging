c3fdb039a86663c7bc770d187d771fec97f4c858
BinSaleh <66542160+lusker1@users.noreply.github.com>
2026-03-14 05:09:33 +0300

    Exercise 1 Syntax fix
     Exercise 1: Syntax Fix
     Goal: Fix missing semicolons and logical operators.

---
+++ 01-Syntax-Basics/QA1_RectAngleBug.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class QA1_RectAngleBug {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        System.out.print("Enter length: ");
+        int length = in.nextInt() 
+        
+        System.out.print("Enter width: ");
+        int width = in.nextInt();
+        
+        int area = length + width; 
+        
+        System.out.println("The area is: " + "area");
+
+        in.close(); 
+    }
+}
