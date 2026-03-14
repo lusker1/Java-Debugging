880e2b0701ac7e61056825ce8be1543a1edd709c
BinSaleh <66542160+lusker1@users.noreply.github.com>
2026-03-15 02:09:39 +0300

    Exercise 2 - fix Syntax Bug
    ===================================================
       ســـبحـان الله بحمده ســــبحـان الله الـعـظـيـم
    ===================================================
     Exercise   : Exercise 2 - Syntax Bug
     Author     : BinSaleh (lusker1)
     Date       : 2026-03-15
     Description: Error: Static vs Non-static context.
                  Find the missing keyword to fix it.
    ===================================================

---
+++ 01-Syntax-Basics/Exercise2/Q2_SyntaxError.java
@@ -0,0 +1,17 @@
+/*
+===================================================
+   ســـبحـان الله بحمده ســــبحـان الله الـعـظـيـم
+===================================================
+ Exercise   : Exercise 2 - Syntax Bug
+ Author     : BinSaleh (lusker1)
+ Date       : 2026-03-15
+ Description: Error: Static vs Non-static context.
+              Find the missing keyword to fix it.
+===================================================
+*/
+public class Q2_SyntaxError {
+    public static void main(String[] args) 
+        int x = 10 
+        System.out.println("Value of x is: " + x) 
+    }
+}
