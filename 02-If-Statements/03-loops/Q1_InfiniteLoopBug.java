7163f28c362a70f901ff519d5a211c9c82dd2714
BinSaleh <66542160+lusker1@users.noreply.github.com>
2026-03-14 05:49:44 +0300

    Exercise 1: Loop Control
    Exercise 1: Loop Control
    Goal: Prevent infinite loops by managing counters.

---
+++ 02-If-Statements/03-loops/Q1_InfiniteLoopBug.java
@@ -0,0 +1,10 @@
+public class Q1_InfiniteLoopBug {
+    public static void main(String[] args) {
+        int i = 1;
+
+        while (i <= 5) {
+            System.out.println("Number: " + i);
+            
+        }
+    }
+}
