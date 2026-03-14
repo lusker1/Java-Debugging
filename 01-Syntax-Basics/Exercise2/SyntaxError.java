6f2716e1d5739dc4cd675f8714b0b20d7cc85272
BinSaleh <66542160+lusker1@users.noreply.github.com>
2026-03-15 02:12:03 +0300

    Exercise 2 solution
     Exercise      : Exercise 2 - Syntax Error Debugging
     Author        : BinSaleh (lusker1)
     Date            :  2026-03-14
     Description : Fixing syntax errors by adding missing semicolons to the Java statements.

---
+++ 01-Syntax-Basics/Exercise2/SyntaxError.java
@@ -0,0 +1,23 @@
+/*
+===================================================
+   ســـبحـان الله بحمده ســــبحـان الله الـعـظـيـم
+===================================================
+ Exercise   : Exercise 2 - Syntax Error Debugging
+ Author     : BinSaleh (lusker1)
+ Date       : 2026-03-14
+ Description: Fixing syntax errors by adding missing 
+              semicolons to the Java statements.
+===================================================
+*/
+
+public class SyntaxError {
+    public static void main(String[] args) {
+        
+        int x = 10;
+        
+        System.out.println("-------------------------");
+        System.out.println("    Value of x is: " + x);
+        System.out.println("-------------------------");
+        
+    }
+}
