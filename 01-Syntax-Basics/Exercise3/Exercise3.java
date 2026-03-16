bdbdc43a6f877a731c87eeaf8fd18e8924b4c478
BinSaleh <66542160+lusker1@users.noreply.github.com>
2026-03-16 19:42:03 +0300

    Exercise 3 - Syntax Challenge
    ===================================================
       ســـبحـان الله بحمده ســــبحـان الله الـعـظـيـم
    ===================================================
     Exercise   : Exercise 3 - Syntax Challenge
     Author     : BinSaleh (lusker1)
     Date       : 2026-03-16
    ===================================================

--- 01-Syntax-Basics/Exercise3/Exercise3.java
+++ 01-Syntax-Basics/Exercise3/Exercise3.java
@@ -10,10 +10,15 @@
 public class Exercise3 {
     public static void main(String[] args) {
         
-        int 1stNumber = 100; 
-        double price = 19.99
-        int total = price * 2;
-        
+        // int 1stNumber = 100;  you can't begin inetger (name) numbers like : 1stNumber that's will be The Type is : Syntax Error 
+        int number1st = 100 ; //correct Syntax  
+
+        // double price = 19.99 , , , forgeting semicolon (;) , the Type is Syntax Error 
+        double price = 19.99 ; // correct Syntax         
+
+        // int total = price * 2 ;   cannot be used (int) bcz price Equal = 19.99 (.99) that's why cannot be use int 
+        double total = price * 2 ;
+
         System.out.println("Total: " + total);
     }
 }
