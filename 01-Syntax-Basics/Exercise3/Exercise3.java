f5a6c4731d2bea4561a944b0a2111523b8a064fb
BinSaleh <66542160+lusker1@users.noreply.github.com>
2026-03-16 06:44:50 +0300

    Exercise 3 - Syntax Challenge
    ===================================================
       ســـبحـان الله بحمده ســــبحـان الله الـعـظـيـم
    ===================================================
     Exercise    : Exercise 3 - Syntax Challenge
     Author      : BinSaleh (lusker1)
     Date         : 2026-03-16
    ==========================================

---
+++ 01-Syntax-Basics/Exercise3/Exercise3.java
@@ -0,0 +1,19 @@
+/*
+===================================================
+   ســـبحـان الله بحمده ســــبحـان الله الـعـظـيـم
+===================================================
+ Exercise   : Exercise 3 - Syntax Challenge
+ Author     : BinSaleh (lusker1)
+ Date       : 2026-03-16
+===================================================
+*/
+public class Exercise3 {
+    public static void main(String[] args) {
+        
+        int 1stNumber = 100; 
+        double price = 19.99
+        int total = price * 2;
+        
+        System.out.println("Total: " + total);
+    }
+}
