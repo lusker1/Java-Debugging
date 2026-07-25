70fb5e5df24f03d11b809d225f9d998fd636ad8d
BinSaleh <66542160+lusker1@users.noreply.github.com>
2026-07-25 14:51:38 +0300

    solution

---
+++ 04-methods/Exercise1/lab5Fixed.java
@@ -0,0 +1,18 @@
+public class lab5Fixed{ 
+   
+  public static void main(String[] args) {
+    
+   
+     
+    int num1 = 25, num2 = 15; 
+     
+    int result = addIntegerNumbers(num1,num2)  ;
+    System.out.println("Sum is: " + result); 
+  
+} 
+  public static int addIntegerNumbers(int a, int b) { 
+    int sum = a + b ; 
+    
+    return sum; 
+  } 
+}
