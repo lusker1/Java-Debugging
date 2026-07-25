c37305fb643a14d14b9f2c3687da1d289e60e1b2
BinSaleh <66542160+lusker1@users.noreply.github.com>
2026-07-25 14:50:38 +0300

    Implement addIntegerNumbers method and fix main
    Fix method to return sum of two integers and update main method to call it correctly.

---
+++ 04-methods/Exercise1/Lab5Original.java
@@ -0,0 +1,14 @@
+public class lab5{ 
+   
+  public void addIntegerNumbers(int a, int b) { 
+    int sum = a + b; 
+    return sum; 
+   
+  public static void main(String[] args) { 
+     
+    double num1 = 25, num2 = 15; 
+ 
+    int result = addIntegerNumbers (num1, num2)  
+    System.out.println("Sum is: " + result); 
+  } 
+} 
